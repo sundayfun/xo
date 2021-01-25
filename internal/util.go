bc935b67d845026351d0bbe467c4a7ed78c89526
heimonsy <heimonsy@gmail.com>
2021-01-25 20:17:09 +0800

    fix(xo): compatbile for group

--- internal/util.go
+++ internal/util.go
@@ -238,6 +238,14 @@ func SingularizeIdentifier(s string) string {
 	return snaker.SnakeToCamelIdentifier(s)
 }
 
+// 因为有个服务的名字叫 Group 和 Group Service 冲突了，这里改为 MGroup
+func GroupCompatible(args *ArgType, name string) string {
+	if len(args.RpcProtoPathPrefix) > 0 && name == "Group" {
+		return "MGroup"
+	}
+	return name
+}
+
 // TBuf is to hold the executed templates.
 type TBuf struct {
 	TemplateType TemplateType
